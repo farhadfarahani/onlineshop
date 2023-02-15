0823aa320b0671f110f0576d77ec50b289f46608
Farhad <farhadfarahani@outlook.com>
2023-02-15 12:57:24 +0330

    create comment form

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
 
-from .models import Product
+from .models import Product, Comment
+from .forms import CommentForm
 
 
 class ProductListView(generic.ListView):
@@ -14,3 +15,17 @@ class ProductDetailView(generic.DetailView):
     model = Product
     template_name = 'products/product_detail.html'
     context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
+
+
+class CommentCreateView(generic.CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.author = self.request.user
